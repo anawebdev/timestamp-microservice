5ddecdecc0151ce532ca95289cb93fd3660b9890
Ana <ana.toma92@gmail.com>
2017-08-09 19:12:14 +0300

    fix bug

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# Backend Challenges boilerplate - package.json
+![](http://i.imgur.com/MImgpQA.jpg)
+
+
+### Description
+
+Unsure if the people walking down the street are wearing big scarves as a fashion statement or because it actually is cold? Can't tell if you should take your umbrella with you when you leave your house? Should you wear two layers of clothes or three? This app can help you anwswer all these questions with an accurate, location based update on current weather.
+
+# Timestamp microservice
+
+- - -
+
+### User Stories
+
+1. I can pass a string as a parameter, and it will check to see whether that string contains either a unix timestamp or a natural language date (example: December 15, 2015)
+2. If it does, it returns both the Unix timestamp and the natural language form of that date.
+3. If it does not contain a date or Unix timestamp, it returns null for those properties.
+- - -
+
+### Technologies used
+
+- HTML
+- CSS
+- Node.js
+- Express.js
+- Moment.js
+
+- - -
+
+*This is a **FreeCodeCamp** assignment.*
--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 var express = require('express');
-var path = require('path');
 var fs = require('fs');
+var app = express();
 var moment = require('moment');
 var port = process.env.PORT || 3000;
 
-var app = express();
-
+var natural, unix = null;
 
 // from boilerplate !!!
+
+if (!process.env.DISABLE_XORIGIN) {
+  app.use(function(req, res, next) {
+    var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
+    var origin = req.headers.origin || '*';
+    if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
+         console.log(origin);
+         res.setHeader('Access-Control-Allow-Origin', origin);
+         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    }
+    next();
+  });
+}
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.route('/_api/package.json')
@@ -28,20 +41,29 @@ app.route('/')
 
 app.get('/:timestamp', function(req, res){
     var string = req.params.timestamp;
-    var time = moment(req.params.timestamp, 'MMMM DD, YYYY', true);
-    if(!time.isValid()){
-        time = moment.unix(req.params.timestamp);     
+    var time = moment(string, 'MMMM DD, YYYY', true);
+    if(time.isValid()){
+        natural = string;
+        unix = moment().unix(string);
+            
     }
     if(!time.isValid()){
-        res.json({
-        "unix" : null,
-        "natural": null
-        });
+
+        var check = moment.unix(parseInt(string));
+
+        if(check.isValid()){
+            natural = moment(check).format("MMMM DD, YYYY");
+            unix = string;
+        } else {
+            natural = null;
+            unix = null;
+        }
+        
     }
 
     res.json({
-        "unix" : time.format('X'),
-        "natural": moment(time).format("MMMM DD, YYYY")
+        "unix" : unix,
+        "natural" : natural
     });
 
 });
